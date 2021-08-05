c98e7ec69c1c958b830b6023187c364fef096e51
Alex <aleksas@mistyblue.tech>
2021-08-05 04:36:31 +0100

    added initial files

---
+++ My Project/assets/char/char.go
@@ -0,0 +1,61 @@
+components {
+  id: "char"
+  component: "/assets/char/char.script"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "body"
+  type: "model"
+  data: "mesh: \"/assets/test/sphere.dae\"\n"
+  "material: \"/assets/test/unlit.material\"\n"
+  "textures: \"/assets/test/purple_checkerboard.jpg\"\n"
+  "skeleton: \"\"\n"
+  "animations: \"\"\n"
+  "default_animation: \"\"\n"
+  "name: \"unnamed\"\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 3.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "cylinder"
+  type: "model"
+  data: "mesh: \"/assets/test/cylinder.dae\"\n"
+  "material: \"/assets/test/unlit.material\"\n"
+  "textures: \"/assets/test/white.jpg\"\n"
+  "skeleton: \"\"\n"
+  "animations: \"\"\n"
+  "default_animation: \"\"\n"
+  "name: \"unnamed\"\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
