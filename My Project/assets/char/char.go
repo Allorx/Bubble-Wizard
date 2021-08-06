d161373948a7d89d94975444ec2efee9ff5b264e
Alex <aleksas@mistyblue.tech>
2021-08-06 03:00:25 +0100

    added stairs
    - stairs.go and core stair code/functionality added + start platform.go

--- My Project/assets/char/char.go
+++ My Project/assets/char/char.go
@@ -59,3 +59,21 @@ embedded_components {
     w: 1.0
   }
 }
+embedded_components {
+  id: "stair_factory"
+  type: "factory"
+  data: "prototype: \"/assets/stairs/stair.go\"\n"
+  "load_dynamically: false\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
---
+++ My Project/assets/start_platform.go
@@ -0,0 +1,23 @@
+embedded_components {
+  id: "model"
+  type: "model"
+  data: "mesh: \"/assets/test/stair.dae\"\n"
+  "material: \"/assets/test/unlit.material\"\n"
+  "textures: \"/assets/test/white.jpg\"\n"
+  "skeleton: \"\"\n"
+  "animations: \"\"\n"
+  "default_animation: \"\"\n"
+  "name: \"unnamed\"\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
