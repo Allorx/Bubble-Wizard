2104dc1e7aeef2fa8379cc35294adfbb556a2e7a
Alex <aleksas@mistyblue.tech>
2021-08-29 12:26:20 +0100

    changed type of game
    - many changes to convert to different game idea

--- My Project/assets/char/char.go
+++ My Project/assets/char/char.go
@@ -13,39 +13,13 @@ components {
     w: 1.0
   }
 }
-embedded_components {
-  id: "body"
-  type: "model"
-  data: "mesh: \"/assets/test/sphere.dae\"\n"
-  "material: \"/assets/test/unlit.material\"\n"
-  "textures: \"/assets/test/purple_checkerboard.jpg\"\n"
-  "skeleton: \"\"\n"
-  "animations: \"\"\n"
-  "default_animation: \"\"\n"
-  "name: \"unnamed\"\n"
-  ""
+components {
+  id: "main_char"
+  component: "/assets/char/main_char.model"
   position {
     x: 0.0
     y: 0.0
     z: 3.0
-  }
-  rotation {
-    x: 0.0
-    y: 0.0
-    z: 0.0
-    w: 1.0
-  }
-}
-embedded_components {
-  id: "stair_factory"
-  type: "factory"
-  data: "prototype: \"/assets/stairs/stair.go\"\n"
-  "load_dynamically: false\n"
-  ""
-  position {
-    x: 0.0
-    y: 0.0
-    z: 0.0
   }
   rotation {
     x: 0.0
