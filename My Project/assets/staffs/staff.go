fa9be338af6688572e13e82b5180323208955e03
Alex <aleksas@mistyblue.tech>
2021-10-19 13:45:14 +0100

    added more models

--- My Project/assets/staffs/staff.go
+++ My Project/assets/staffs/staff.go
@@ -21,3 +21,26 @@ embedded_components {
     w: 0.70710677
   }
 }
+embedded_components {
+  id: "stone"
+  type: "model"
+  data: "mesh: \"/assets/staffs/staff_stone.dae\"\n"
+  "material: \"/assets/shaders/unlit.material\"\n"
+  "textures: \"/assets/staffs/StaffStone_base.png\"\n"
+  "skeleton: \"\"\n"
+  "animations: \"\"\n"
+  "default_animation: \"\"\n"
+  "name: \"unnamed\"\n"
+  ""
+  position {
+    x: 0.0
+    y: 2.5
+    z: 0.02
+  }
+  rotation {
+    x: -0.17703883
+    y: -0.68458545
+    z: 0.17703883
+    w: 0.68458545
+  }
+}
