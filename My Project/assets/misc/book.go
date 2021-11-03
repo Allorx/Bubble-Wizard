ce3c6d088ff4796df1287fa5adb8a046e6edc423
Alex <aleksas@mistyblue.tech>
2021-11-03 15:42:24 +0000

    added art

---
+++ My Project/assets/misc/book.go
@@ -0,0 +1,46 @@
+embedded_components {
+  id: "model"
+  type: "model"
+  data: "mesh: \"/assets/misc/book2.dae\"\n"
+  "material: \"/assets/shaders/lit.material\"\n"
+  "textures: \"/assets/misc/book2_base.png\"\n"
+  "skeleton: \"\"\n"
+  "animations: \"\"\n"
+  "default_animation: \"\"\n"
+  "name: \"unnamed\"\n"
+  ""
+  position {
+    x: 3.0
+    y: -0.1
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "model1"
+  type: "model"
+  data: "mesh: \"/assets/misc/book.dae\"\n"
+  "material: \"/assets/shaders/lit.material\"\n"
+  "textures: \"/assets/misc/book_base.png\"\n"
+  "skeleton: \"\"\n"
+  "animations: \"\"\n"
+  "default_animation: \"\"\n"
+  "name: \"unnamed\"\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.4
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.13052619
+    z: 0.0
+    w: 0.9914449
+  }
+}
---
+++ My Project/assets/misc/candle/candle.go
@@ -0,0 +1,53 @@
+components {
+  id: "fire"
+  component: "/assets/misc/fire.particlefx"
+  position {
+    x: 0.0
+    y: 3.5
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+components {
+  id: "candle"
+  component: "/assets/misc/candle/candle.script"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "model"
+  type: "model"
+  data: "mesh: \"/assets/misc/candle/candle.dae\"\n"
+  "material: \"/assets/shaders/lit.material\"\n"
+  "textures: \"/assets/misc/candle/candle_base.png\"\n"
+  "skeleton: \"\"\n"
+  "animations: \"\"\n"
+  "default_animation: \"\"\n"
+  "name: \"unnamed\"\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
