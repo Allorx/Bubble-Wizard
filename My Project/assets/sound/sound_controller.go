9d7db425a7b52a1c4a1bd58abec03c3abfeb13f0
GCPD1 <samer712@hotmail.co.uk>
2021-11-05 17:51:53 +0000

    Added Sound Controller
    Added temporary background music.

---
+++ My Project/assets/sound/sound_controller.go
@@ -0,0 +1,30 @@
+components {
+  id: "sound_controller"
+  component: "/assets/sound/sound_controller.script"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+components {
+  id: "background_music"
+  component: "/assets/sound/background_music.sound"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
