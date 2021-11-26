8c33087466694533fd902b6a2b4e0b5b1b56fcac
Alex <aleksas@mistyblue.tech>
2021-11-26 19:59:33 +0000

    added sfx

--- My Project/assets/sound/sound_controller.go
+++ My Project/assets/sound/sound_controller.go
@@ -43,3 +43,33 @@ components {
     w: 1.0
   }
 }
+components {
+  id: "bubble_echo"
+  component: "/assets/sound/bubble_echo.sound"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+components {
+  id: "bolt"
+  component: "/assets/sound/bolt.sound"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
